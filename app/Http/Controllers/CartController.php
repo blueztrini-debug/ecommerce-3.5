555dafcfeb66bee72cdc7617af456f1c63416941
Darren <blueztrini@gmail.com>
2026-02-04 21:14:05 -0400

    Implemented Cart Functionality
    Added index function to view cart
    Added store function to add to cart
    add deleted function to remove to cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,35 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Traits\PhpFlasher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
 
     //Add flasher here
-
+    use PhpFlasher;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return true;
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+        $user = Auth::user();
+        $cart_data = $user->products()->withPrices()->get();
+        $cart_data->calculateSubtotal();
+        return view('pages.default.cartpage', compact('cart_data'));
     }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        return true;
-    }
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->product_id],
+            ['quantity' => DB::raw('quantity + ' . $request->quantity), 'updated_at' => now()]
+        );
 
-    /**
-     * Store a newly created resource in storage.
-     */
+        /**
+         * Store a newly created resource in storage.
+         */
+    }
     public function addToCartFromStore(Request $request)
     {
-        return true;
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->id],
+            ['quantity' => DB::raw('quantity + ' . 1), 'updated_at' => now()]
+        );
+        $this->flashSuccess('Product added to cart');
+        return redirect()->route('cart.index')->with('message', 'Product added to the cart');
     }
 
 
@@ -39,6 +54,8 @@ class CartController extends Controller
      */
     public function destroy(string $id)
     {
-        return true;
+        Cart::destroy($id);
+        $this->flashError('Product removed from cart');
+        return redirect()->route('cart.index')->with('message', "Product removed from cart");
     }
 }

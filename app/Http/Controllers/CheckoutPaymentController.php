f9312957e4bdd86b529eb966cca0c7cdcea5b1ef
Darren <blueztrini@gmail.com>
2026-02-11 19:51:34 -0400

    Creating Checkout page and begin testingcheckout functionality

--- app/Http/Controllers/CheckoutPaymentController.php
+++ app/Http/Controllers/CheckoutPaymentController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Helpers\ShippingHelper;
+use App\Helpers\StripeCheckout;
+use App\Models\Order;
+
 
 class CheckoutPaymentController extends Controller
 {
@@ -14,7 +19,58 @@ class CheckoutPaymentController extends Controller
      * @return void
      */
     public function index($payment)
-    {
+    { // Get groups
+        $groups = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        // Get user and stores in a variable.
+
+        $user = Auth::user();
+        // Create variables
+        $shipping_helper = new ShippingHelper();
+        $stripe_checkout = new StripeCheckout();
+        $order = new Order();
+        $insert_data = [];
+        $completed = false;
+
+
+        // Get products
+        $cart_data = $user->products()->withPrices()->get();
+
+        // Check if cart is empty
+        if ($cart_data->isEmpty()) {
+            return redirect()->route('cart.index')->with('message', 'Your cart is empty');
+        }
+        // Get Subtotal
+        $cart_data->calculateSubtotal();
+        // Determine payment
+        switch ($payment) {
+            case 'stripe':
+                # code...
+                break;
+
+
+            default:
+            insert_data = [
+                'payment_provider'  => 'testing',
+                'payment_id'        => 'testing
+                };
+                $completed = true;
+                break;
+        }
+
+            // Validate
+        if (!$completed || empty($insert_data)) {
+            dd('Payment is incomplete or checkout is missing');
+
+        }
+
+
+
+        // Create order details
+
+        // Create order details
+
+        // Redirect
         return true;
     }
 }
